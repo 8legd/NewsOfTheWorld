4812b28a31e99fd99b65309c6aed35b6edbc735f
drew <drew@venturesoft.co.uk>
2016-11-21 19:32:29 +0000

    Wait a few seconds for the driver to be up and running - this sometimes takes a while on first load

--- tests/test_news_of_the_world.py
+++ tests/test_news_of_the_world.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import splinter, os
+import splinter, os, urllib2, time
 
 def test_news_of_the_world():
 
+    # if needed wait a few seconds for the driver to be up and running - sometimes takes a while on first load
+    driver_url = "http://seleniumchrome:4444/wd/hub"
+    req = urllib2.Request(driver_url)
+    try: urllib2.urlopen(req)
+    except URLError as e:
+        time.sleep(10)
+
     with splinter.Browser(driver_name="remote",
-             url="http://seleniumchrome:4444/wd/hub",
+             url=driver_url,
              browser="chrome") as browser:
 
         browser.visit("http://www.bbc.co.uk/news/world")
